7d87057f8d0f994dacbe3f482f023b7811ec00e5
mehedi-ice <mhsokal2000@gmail.com>
2026-02-07 21:33:53 +0600

    fox

--- server.js
+++ server.js
@@ -11,10 +11,14 @@ app.use(express.json());
 // app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/checkout_assets', express.static(path.join(__dirname, 'public', 'checkout_assets')));
 
+// Routes MUST come before express.static if you want to override default file serving
+app.get('/checkout', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'checkout.html'));
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
-app.get('/checkout', (req, res) => res.sendFile(path.join(__dirname, 'public', 'checkout.html')));
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
